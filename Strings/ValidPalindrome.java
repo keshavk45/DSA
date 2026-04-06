53999c5524e124b374775ac21a7632e9970331d0
keshavk45 <keshavk0682@gmail.com>
2026-04-06 23:24:49 +0530

    Add ValidPalindrome class to check for palindromes ignoring non-alphanumeric characters and case sensitivity

---
+++ Strings/ValidPalindrome.java
@@ -0,0 +1,28 @@
+//in this problem we are checking palindrome by ignoring non-alphanumeric characters and case sensitivity
+package Strings;
+public class ValidPalindrome {
+    public boolean isPalindrome(String s) {
+        int i = 0;
+        int j = s.length()-1;
+        while(i<j){
+            while(i<j && !Character.isLetterOrDigit(s.charAt(i))){
+                i++;
+            }
+            while(i<j && !Character.isLetterOrDigit(s.charAt(j))){
+                j--;
+            }
+            if(Character.toLowerCase(s.charAt(i))!=Character.toLowerCase(s.charAt(j))){
+                return false;
+            }else{
+                i++;
+                j--;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        ValidPalindrome vp = new ValidPalindrome();
+        System.out.println(vp.isPalindrome("racecar"));
+    }
+}
