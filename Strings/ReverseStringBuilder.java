4cfb82d9c32657961a51de919c789ee5c878239f
keshavk45 <keshavk0682@gmail.com>
2026-04-07 16:15:46 +0530

    Add ReverseStringBuilder class to reverse a string without using inbuilt functions

---
+++ Strings/ReverseStringBuilder.java
@@ -0,0 +1,22 @@
+//Reverse a string without using inbuilt function
+
+package Strings;
+
+public class ReverseStringBuilder {
+    public static void main(String[] args) {
+        String s = "Raghav";
+        StringBuilder sb = new StringBuilder(s);
+        int i = 0;
+        int j = sb.length()-1;
+        while(i<=j){
+            char temp1 = sb.charAt(i);
+            char temp2 = sb.charAt(j);
+            sb.setCharAt(i, temp2);
+            sb.setCharAt(j, temp1);
+            i++;
+            j--;
+
+        }
+        System.out.println(sb);
+    }
+}
