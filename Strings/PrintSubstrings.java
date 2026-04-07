99ce36a1e6356d677793e67be7d46fd4185c6088
keshavk45 <keshavk0682@gmail.com>
2026-04-07 15:30:44 +0530

    Add PrintSubstrings class to print all substrings of a given string

---
+++ Strings/PrintSubstrings.java
@@ -0,0 +1,11 @@
+package Strings;
+public class PrintSubstrings {
+    public static void main(String[] args) {
+        String s = "abc";
+        for(int i=0;i<s.length();i++){
+            for(int j=i+1;j<=s.length();j++){
+                System.out.println(s.substring(i,j));
+            }
+        }
+    }
+}
