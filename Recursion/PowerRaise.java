9a5fa1ae6dbb99acc6aa8691dd08613be2b349e0
keshavk45 <keshavk0682@gmail.com>
2026-04-08 18:58:05 +0530

    Refactor PowerRaise class to remove Math.pow method and implement recursive power calculation

--- Recursion/PowerRaise.java
+++ Recursion/PowerRaise.java
@@ -1,4 +1,27 @@
-//METHOD 1  - DIRECT USING INBUILT MATH.POW METHOD
+// //METHOD 1  - DIRECT USING INBUILT MATH.POW METHOD
+
+// package Recursion;
+// import java.util.*;
+
+// public class PowerRaise {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter base");
+//         int a = sc.nextInt();
+//         System.out.println("enter exponential");
+//         int b = sc.nextInt();
+//         System.out.println(Math.pow(a, b));      
+//     }
+    
+
+    
+// }
+
+
+
+
+
+//METHOD 2 - USING RECURSION
 
 package Recursion;
 import java.util.*;
@@ -10,7 +33,18 @@ public class PowerRaise {
         int a = sc.nextInt();
         System.out.println("enter exponential");
         int b = sc.nextInt();
-        System.out.println(Math.pow(a, b));      
+        System.out.println(pow(a, b)); 
+        
+        
+    }
+
+    public static int pow(int a , int b){
+        if(b==0){
+            return 1;
+        }
+        int res = a*pow(a, b-1);
+        return res;
+
     }
     
 
