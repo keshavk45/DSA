ba161ddff20d9f20288ee4c91648fd0b79c8b0aa
keshavk45 <keshavk0682@gmail.com>
2026-04-08 18:57:18 +0530

    Add PowerRaise class to calculate power using Math.pow method

---
+++ Recursion/PowerRaise.java
@@ -0,0 +1,18 @@
+//METHOD 1  - DIRECT USING INBUILT MATH.POW METHOD
+
+package Recursion;
+import java.util.*;
+
+public class PowerRaise {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter base");
+        int a = sc.nextInt();
+        System.out.println("enter exponential");
+        int b = sc.nextInt();
+        System.out.println(Math.pow(a, b));      
+    }
+    
+
+    
+}
