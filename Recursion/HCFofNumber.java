8e54cd785f332f521797f4b33886a3753a6e1b98
keshavk45 <keshavk0682@gmail.com>
2026-04-09 16:36:55 +0530

    Refactor HCFofNumber class to implement HCF calculation using recursion

--- Recursion/HCFofNumber.java
+++ Recursion/HCFofNumber.java
@@ -1,5 +1,27 @@
 // HCF of two number using loop
 
+// package Recursion;
+// import java.util.*;
+
+// public class HCFofNumber {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+
+//         int num = 0;
+//         for(int i = 1 ; i<a ; i++){
+//             if(a%i==0 && b%i==0){
+//                 num = i;
+//             }
+//         }
+//         System.out.println(num);
+//     }
+    
+// }
+
+//METHOD 2 - USING RECURSION
+
 package Recursion;
 import java.util.*;
 
@@ -8,14 +30,15 @@ public class HCFofNumber {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
+        System.out.println(hcf(a, b));
+    }
 
-        int num = 0;
-        for(int i = 1 ; i<a ; i++){
-            if(a%i==0 && b%i==0){
-                num = i;
-            }
+    public static int hcf(int a , int b){
+        if(a == 0){
+            return b;
         }
-        System.out.println(num);
+        return hcf(b%a, a);
     }
     
 }
+
