6084068492792e0ba0f902b4990ba229bf521651
keshavk45 <keshavk0682@gmail.com>
2026-05-30 10:48:57 +0530

    Add displayQueue method to TraversalofQueue class for enhanced queue traversal

--- Queue/TraversalofQueue.java
+++ Queue/TraversalofQueue.java
@@ -2,6 +2,13 @@ package Queue;
 import java.util.*;
 
 public class TraversalofQueue {
+
+    private static void displayQueue(Queue<Integer> q) {
+        System.out.println("Traversal using for-each loop:");
+        for (int element : q) {
+            System.out.println(element); // Process the element (e.g., print it)
+        }
+    }
     public static void main(String[] args) {
         Queue<Integer> q = new LinkedList<>(); //or Queue<Integer> q = new ArrayDeque<>();
         q.add(1);
