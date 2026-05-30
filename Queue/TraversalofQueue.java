a1e07302987d1a93c2d6e4496d8ee470e981181a
keshavk45 <keshavk0682@gmail.com>
2026-05-30 10:45:37 +0530

    Add TraversalofQueue class with main method demonstrating queue traversal

---
+++ Queue/TraversalofQueue.java
@@ -0,0 +1,22 @@
+package Queue;
+import java.util.*;
+
+public class TraversalofQueue {
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>(); //or Queue<Integer> q = new ArrayDeque<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        
+        // Traversal using for-each loop
+        System.out.println("Traversal using for-each loop:");   
+        int n = q.size();
+        for (int i = 0; i < n; i++) {
+            int element = q.poll(); // Remove the front element
+            System.out.println(element); // Process the element (e.g., print it)
+            q.add(element); // Add the element back to the rear of the queue
+        }
+    }
+
+    
+}
