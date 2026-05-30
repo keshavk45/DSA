f90a78a1b5691838657c05696752dbbe8cdb7d0b
keshavk45 <keshavk0682@gmail.com>
2026-05-30 10:47:33 +0530

    Remove unused traversal method from TraversalofQueue class

--- Queue/TraversalofQueue.java
+++ Queue/TraversalofQueue.java
@@ -2,15 +2,6 @@ package Queue;
 import java.util.*;
 
 public class TraversalofQueue {
-
-    private static void d(Queue<Integer> q) {
-        int n = q.size();
-        for (int i = 0; i < n; i++) {
-            int element = q.poll(); // Remove the front element
-            System.out.println(element); // Process the element (e.g., print it)
-            q.add(element); // Add the element back to the rear of the queue
-        }
-    }
     public static void main(String[] args) {
         Queue<Integer> q = new LinkedList<>(); //or Queue<Integer> q = new ArrayDeque<>();
         q.add(1);
