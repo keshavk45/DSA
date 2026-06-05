55e4093c5e37d28e14d4ff1a349d0c76beae2cbe
keshavk45 <keshavk0682@gmail.com>
2026-06-06 00:51:25 +0530

    Add Node class and implementation for binary tree display

---
+++ Tree/Node.java
@@ -0,0 +1,43 @@
+package Tree;
+
+public class Node {
+    int val;
+    Node Left;
+    Node Right;
+    Node(int val){
+        this.val=val;
+        this.Left=null;
+        this.Right=null;
+    }
+}
+
+class Implementation {
+    private static void display(Node root){
+        if(root==null) return;
+        System.out.print(root.val+" ");
+        display(root.Left); //left ki saari value print kar dega
+        display(root.Right); // right ki saari value print kar dega
+    }
+
+    public static void main(String[] args) {
+        //        1
+        //      /   \
+        //     2     3
+        //    / \   / \
+        //   4   5 6   7
+        Node a = new Node(1);
+        Node b = new Node(2);
+        Node c = new Node(3);
+        Node d = new Node(4);   
+        Node e = new Node(5);
+        Node f = new Node(6);
+        Node g = new Node(7);
+        a.Left=b;
+        a.Right=c;  
+        b.Left=d;
+        b.Right=e;
+        c.Left=f;
+        c.Right=g;
+        display(a);
+    }
+}
